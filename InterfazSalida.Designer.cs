8080e0b321f8cd1425856e820f5c823d10642301
Nicolas Olivares <nicolasolivares097@gmail.com>
2022-07-23 03:37:24 -0400

    Agregar archivos de proyecto.

---
+++ InterfazSalida.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace AppInventario
+{
+    partial class InterfazSalida
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumS = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Sku = new System.Windows.Forms.Label();
+            this.txtSku = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCantSal = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número de salida ";
+            // 
+            // txtNumS
+            // 
+            this.txtNumS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumS.Location = new System.Drawing.Point(226, 47);
+            this.txtNumS.Name = "txtNumS";
+            this.txtNumS.Size = new System.Drawing.Size(129, 26);
+            this.txtNumS.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fecha";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CalendarMonthBackground = System.Drawing.SystemColors.Info;
+            this.dateTimePicker.CalendarTrailingForeColor = System.Drawing.SystemColors.ControlText;
+            this.dateTimePicker.Location = new System.Drawing.Point(226, 115);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(195, 20);
+            this.dateTimePicker.TabIndex = 3;
+            this.dateTimePicker.Value = new System.DateTime(2022, 7, 23, 0, 0, 0, 0);
+            // 
+            // Sku
+            // 
+            this.Sku.AutoSize = true;
+            this.Sku.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sku.Location = new System.Drawing.Point(43, 188);
+            this.Sku.Name = "Sku";
+            this.Sku.Size = new System.Drawing.Size(40, 20);
+            this.Sku.TabIndex = 4;
+            this.Sku.Text = "Sku";
+            // 
+            // txtSku
+            // 
+            this.txtSku.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSku.Location = new System.Drawing.Point(226, 182);
+            this.txtSku.Name = "txtSku";
+            this.txtSku.Size = new System.Drawing.Size(129, 26);
+            this.txtSku.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cantidad Salida";
+            // 
+            // txtCantSal
+            // 
+            this.txtCantSal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantSal.Location = new System.Drawing.Point(226, 255);
+            this.txtCantSal.Name = "txtCantSal";
+            this.txtCantSal.Size = new System.Drawing.Size(129, 26);
+            this.txtCantSal.TabIndex = 7;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(104, 353);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(75, 28);
+            this.btnValidar.TabIndex = 8;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(280, 353);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 28);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // InterfazSalida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(459, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtCantSal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSku);
+            this.Controls.Add(this.Sku);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumS);
+            this.Controls.Add(this.label1);
+            this.Name = "InterfazSalida";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pantalla Salida";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label Sku;
+        private System.Windows.Forms.TextBox txtSku;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCantSal;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ InterfazSalida.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppInventario
+{
+    public partial class InterfazSalida : Form
+    {
+        ServiceReference.WebServiceSoapClient auxServicio = new ServiceReference.WebServiceSoapClient();
+        public InterfazSalida()
+        {
+            InitializeComponent();
+            dateTimePicker.Enabled = false; 
+            btnGuardar.Enabled = false; 
+        }
+
+
+        
+
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+
+            ValidarStock();
+        }
+
+        private void Insertar()
+        {
+            ServiceReference.SalidaProducto auxSale = new ServiceReference.SalidaProducto();
+            auxSale.numSalida = Convert.ToInt32(txtNumS.Text);
+            auxSale.sku = txtSku.Text;
+            auxSale.cantidad_salida = Convert.ToInt32(txtCantSal.Text);
+
+
+
+            if (auxServicio.insertarService(auxSale))
+            {
+                auxServicio.serviceActualizaC(txtSku.Text, Convert.ToInt32(txtCantSal.Text));
+                MessageBox.Show("Se agregarón los datos");
+                btnGuardar.Enabled=false;   
+
+            }
+            else
+            {
+                MessageBox.Show("No se ha agregado nada");
+            }
+        }
+
+        private bool ValidarStock()
+        {
+            if (Validacion())
+            {
+                if (Convert.ToInt32(txtCantSal.Text) > auxServicio.traerStock(txtSku.Text))
+                {
+                    MessageBox.Show("La cantidad a sacar es mayor a la que se encuentrad disponible");
+                    
+                }
+                else
+                {
+                    MessageBox.Show("se encuentra la cantidad, deseada puede sacar lo solicitado");
+                    btnGuardar.Enabled = true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool Validacion()
+        {
+
+            if(txtNumS.Text == string.Empty)
+            {
+                MessageBox.Show("Debe Agregar el número de salida");
+                
+            }else if(txtSku.Text == string.Empty)
+            {
+                MessageBox.Show("Debe agregar el Sku del producto");
+
+            }else if(txtCantSal.Text == string.Empty){
+
+                MessageBox.Show("Debe ingresar la cantidad de salida");
+            }
+            else
+            {
+                return true;
+            }
+
+            return false;   
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Insertar();
+        }
+    }
+}
